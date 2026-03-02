b040e77f0500a116070b6ddc0a3a14f4ca719b9f
Zamzam <zamzamqandid@gmail.com>
2026-03-02 04:55:18 -0500

    Fix TutorialViewer warning

--- src/screens/TutorialViewer.js
+++ src/screens/TutorialViewer.js
@@ -14,6 +14,7 @@ const TutorialViewer = () => {
 
   useEffect(() => {
     loadTutorial();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const loadTutorial = async () => {
